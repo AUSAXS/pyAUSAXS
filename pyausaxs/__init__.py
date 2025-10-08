ad57b766eda8e8e3bcea1ef3c1e7af1e1a1d4578
krellemeister <k.relle@live.dk>
2025-10-08 07:52:40 +0200

    v1.0.3

--- pyausaxs/__init__.py
+++ pyausaxs/__init__.py
@@ -3,5 +3,5 @@
 from .wrapper import AUSAXS, AUSAXSManualFit, ausaxs, create_ausaxs
 
 __all__ = ["AUSAXS", "AUSAXSManualFit", "ausaxs", "create_ausaxs"]
-__version__ = "1.1.0"
+__version__ = "1.0.3"
 ausaxs = ausaxs
