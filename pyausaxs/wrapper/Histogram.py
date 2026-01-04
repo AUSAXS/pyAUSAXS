7c3d9684580ffe3d7d0ecf09f00ca2e70acc93fe
Krelle <kristian.lytje@uni-saarland.de>
2026-01-04 17:08:40 +0100

    added truncate method to Histogram

--- pyausaxs/wrapper/Histogram.py
+++ pyausaxs/wrapper/Histogram.py
@@ -7,6 +7,19 @@ class Histogram:
         self._aa = aa
         self._aw = aw
         self._ww = ww
+    
+    def truncate(self) -> None:
+        """
+        Truncate the data to remove all zero-values following the last nonzero index. 
+        The resulting data will still have the same size. 
+        """
+        for i in range(len(self._bins)-1, 1, -1):
+            if self._aa[i] + self._aw[i] + self._ww[i] != 0:
+                break
+        self._bins = self._bins[:i]
+        self._aa = self._aa[:i]
+        self._aw = self._aw[:i]
+        self._ww = self._ww[:i]
 
     def counts_aa(self) -> np.ndarray:
         """The atomic-atomic distance counts."""
